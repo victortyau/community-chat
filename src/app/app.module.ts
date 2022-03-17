3db07e0f716fca6e26976bcbbc27e05f236cc68a
Victor Tejada Yau <victor.tejada@arcadia.com>
2022-03-16 22:30:46 -0500

    working on community chat project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { UsersComponent } from './users/users.component';
+import { ChannelsComponent } from './channels/channels.component';
+import { ContainerComponent } from './container/container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    UsersComponent,
+    ChannelsComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,
